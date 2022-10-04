0fe198c1e8b7a7d428df3a611669c3644aea7daf
NeNe-Baby <pfxuchn@gmail.com>
2022-10-04 10:23:53 +0800

    添加登录组件

--- src/components/common/LoginPage.vue
+++ src/components/common/LoginPage.vue
@@ -10,12 +10,58 @@
     </el-input>
   </div>
 
+  <el-button type="primary" round @click="submit">登录</el-button>
+
 </template>
 
 <script>
+
+import { ref } from 'vue'
+import http from '@/request/index'
+import router from "@/router";
+
 export default {
-  name: "LoginPage"
+  name: "LoginPage",
+  setup(){
+    const userName = ref('')
+    const password = ref('')
+    function submit(){
+      http.post('http://localhost:9527/api/user/login', JSON.stringify({
+        "name": userName.value,
+        "password": password.value
+      })).then(data=>{
+
+        if (data.success){
+          localStorage.setItem("token",data.obj)
+
+          router.push('/')
+
+
+        } else {
+          alert(data.message)
+        }
+
+
+      })
+
+    }
+
+
+    return {
+      userName,
+      password,
+      submit
+    }
+  }
 }
+
+
+
+
+
+
+
+
 </script>
 
 <style scoped>
