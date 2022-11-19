2cab8359193831635815233a90c33938fb46585e
pf_xu <pf.xu@foxmail.com>
2022-11-19 18:56:51 +0800

    修正脚本

--- sh/StartImgAdmin.sh
+++ sh/StartImgAdmin.sh
@@ -1,13 +1,14 @@
 TAG=$1
 docker pull pfxuresources/img-admin:"$TAG"
-containerId=`docker ps | grep img-admin-serivce | awk '{print $1}'`
+containerId=$(docker ps | grep img-admin-serivce | awk '{print $1}')
 if [ -n "$containerId" ]
     then
       docker stop "$containerId"
       docker rm "$containerId"
 fi
 
-docker rmi `docker images | grep none | awk '{print $3}'`
+# shellcheck disable=SC2046
+docker rmi $(docker images | grep none | awk '{print $3}')
 
 docker run -it -d -p 9528:80 \
         --name img-admin-serivce \
